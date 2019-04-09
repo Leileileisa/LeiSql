4437d992505839c1eede72775485c119ad575ea4
Leisa <1015375776@qq.com>
2019-04-09 22:32:11 +0800

    2019年4月9日

--- LeiSql/LeiSql/BPlus.h
+++ LeiSql/LeiSql/BPlus.h
@@ -2,14 +2,17 @@
 #ifndef BPLUS_H
 #define BPLUS_H
 #include <memory>
+#include <new>
+#define PAGESIZE 4096
 namespace LeiSQL
 {
     class BPlus
     {
+        class TreeNode;
         class DataNode
         {
         public:
-            DataNode(char* username,char* passwd,DataNode* childNode=nullptr) :Username(username),Passwd(passwd),ChildNode(childNode)
+            DataNode(int uid,const char* username,const char* passwd,TreeNode* childNode=nullptr) :UID(uid),Username(username),Passwd(passwd),ChildNode(childNode)
             {
 
             }
@@ -21,36 +24,67 @@ namespace LeiSQL
             {
                 return UID;
             }
+            TreeNode* GetChild()
+            {
+                return ChildNode;
+            }
+            void SetChild(TreeNode* dataNode)
+            {
+                ChildNode = dataNode;
+            }
         private:
-            int UID;
-            char* Username;
-            char* Passwd;
-            DataNode* ChildNode;
+            long UID;//8
+            const char* Username;//8
+            const char* Passwd;//8
+            TreeNode* ChildNode;//8
         };
         class TreeNode
         {
         public:
-            TreeNode():PtrStart(malloc(4096)),PtrNow(PtrStart),PtrEnd(PtrStart+4096)
+            TreeNode():PtrStart(malloc(PAGESIZE)),PtrNow(PtrStart)
             {
-                
+                PtrEnd = (void*)(long(PtrStart)+PAGESIZE);
             }
-            void* Build(int uid, const char* username, const char* passwd)
+            void* Build(long uid, const char* username, const char* passwd)
             {
                 int length;
-                if (PtrEnd - PtrNow < length)
+                if ((long)PtrEnd-(long)PtrNow<sizeof(DataNode))
                 {
-
+                    DataNode* node = Find(FirstNode, uid);
+                    LastNode = new(PtrNow) DataNode(uid,username,passwd);
                 }
                 else
                 {
 
                 }
             }
+            
+            DataNode* Find(DataNode* firstNode, long uid)
+            {
+                DataNode* node = firstNode;
+                DataNode* nextNode = node + sizeof(DataNode);
+                DataNode* lastNode = firstNode + PAGESIZE - sizeof(DataNode);
+                while ((node->GetUID() < uid)&&(nextNode<lastNode))
+                {
+                    node+=sizeof(DataNode);
+                    nextNode += sizeof(DataNode);
+                    if (nextNode->GetUID() > uid)
+                    {
+                        break;
+                    }
+                }
+                if (nextNode == lastNode&&)
+                {
+
+                }
+                
+            }
         private:
             void* PtrStart;
             void* PtrNow;
             void* PtrEnd;
             DataNode* FirstNode;
+            DataNode* LastNode;
         };
         
     public:
